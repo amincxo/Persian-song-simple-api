755c5ff1f0e7567ebde781ad251a16e91bd9d75e
unknown <amincxo@gmail.com>
2024-09-29 00:01:55 +0330

    add all song persian

---
+++ faallsong.js
@@ -0,0 +1,24 @@
+
+const faallsong = 
+    {
+        name: 'همه اهنگ ها',
+        songs: [
+            {  id:1 , name: 'درستش کن', artist: 'بازی سرد', src: "ColdplayFixYou.mp3" , format: 'mp3' , cover: 'covers/ColdplayFixYou.jpg' },
+            {  id:2 , name: 'زرد', artist: 'بازی سرد', src: "ColdplayYellow.mp3" , format: 'mp3' , cover: 'covers/ColdplayYellow.jpg' },
+            {  id:3 , name: 'دانشمند', artist: 'بازی سرد', src: "coldplaythescientist.mp3" , format: 'mp3' , cover: 'covers/thescientist.jpg' },
+            {  id:4 , name: 'بچه سوسول', artist: 'محسن لرستانی', src: "mohsenBachesosol.mp3" , format: 'mp3' , cover: 'covers/mohsenBachesosol.jpg' },
+            {  id:5 , name: 'بچه قرتی', artist: 'محسن لرستانی', src: "MohsenLorestaniBacheGherti.mp3" , format: 'mp3' , cover: 'covers/mohsenBachegherti.jpg' },
+            {  id:6 , name: 'بچه ننه', artist: 'محسن لرستانی', src: "MohsenLorestaniBacheNane.mp3" , format: 'mp3' , cover: 'covers/MohsenLorestaniBacheNane.jpg' },
+            {  id:7 , name: 'بی کس', artist: 'محسن لرستانی', src: "MohsenLorestaniBiKas.mp3" , format: 'mp3' , cover: 'covers/bikasmohsen.jpg' },
+            {  id:8 , name: 'نگین', artist: 'محسن لرستانی', src: "MohsenLorestaniNegin.mp3" , format: 'mp3' , cover: 'covers/MohsenLorestaniNegin.jpg' },
+            {  id:9 , name: 'فرشته', artist: 'رامشتاین', src: "rammsteinengel.mp3" , format: 'mp3' , cover: 'covers/EngelRammstein.jpg' },
+            {  id:10 , name: 'بدون آتش', artist: 'رامشتاین', src: "rammsteinfeuer_frei.mp3" , format: 'mp3' , cover: 'covers/Feuerfrei.jpg' },
+            {  id:11 , name: 'من می خواهم', artist: 'رامشتاین', src: "rammstineich_will.mp3" , format: 'mp3' , cover: 'covers/ichwillramshtine.jpg' },
+        ]
+    }
+   
+;
+
+module.exports = faallsong;
+
+
