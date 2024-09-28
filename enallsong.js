4a5731d2be5a50b60df8ece01fe82ccd125d39ed
unknown <amincxo@gmail.com>
2024-09-29 00:01:30 +0330

    add all song english

---
+++ enallsong.js
@@ -0,0 +1,24 @@
+
+const enallsong = 
+    {
+        name: 'All Song',
+        songs: [
+            {  id:1 , name: 'Fix You', artist: 'Coldplay', src: "ColdplayFixYou.mp3" , format: 'mp3' , cover: 'covers/ColdplayFixYou.jpg' },
+            {  id:2 , name: 'Yellow', artist: 'Coldplay', src: "ColdplayYellow.mp3" , format: 'mp3' , cover: 'covers/ColdplayYellow.jpg' },
+            {  id:3 , name: 'TheScientist', artist: 'Coldplay', src: "coldplaythescientist.mp3" , format: 'mp3' , cover: 'covers/thescientist.jpg' },
+            {  id:4 , name: 'Bache Sosol', artist: 'Mohsen Lorestani', src: "mohsenBachesosol.mp3" , format: 'mp3' , cover: 'covers/mohsenBachesosol.jpg' },
+            {  id:5 , name: 'Bache Gherti', artist: 'Mohsen Lorestani', src: "MohsenLorestaniBacheGherti.mp3" , format: 'mp3' , cover: 'covers/mohsenBachegherti.jpg' },
+            {  id:6 , name: 'Bache Naneh', artist: 'Mohsen Lorestani', src: "MohsenLorestaniBacheNane.mp3" , format: 'mp3' , cover: 'covers/MohsenLorestaniBacheNane.jpg' },
+            {  id:7 , name: 'Bi Kas', artist: 'Mohsen Lorestani', src: "MohsenLorestaniBiKas.mp3" , format: 'mp3' , cover: 'covers/bikasmohsen.jpg' },
+            {  id:8 , name: 'Negin', artist: 'Mohsen Lorestani', src: "MohsenLorestaniNegin.mp3" , format: 'mp3' , cover: 'covers/MohsenLorestaniNegin.jpg' },
+            {  id:9 , name: 'Engel', artist: 'Rammstein', src: "rammsteinengel.mp3" , format: 'mp3' , cover: 'covers/EngelRammstein.jpg' },
+            {  id:10 , name: 'feuer frei', artist: 'Rammstein', src: "rammsteinfeuer_frei.mp3" , format: 'mp3' , cover: 'covers/Feuerfrei.jpg' },
+            {  id:11 , name: 'ich will', artist: 'Rammstein', src: "rammstineich_will.mp3" , format: 'mp3' , cover: 'covers/ichwillramshtine.jpg' },
+        ]
+    }
+   
+;
+
+module.exports = enallsong;
+
+
