fd27f42be3517f1348cb98ad3c7047138f297e07
unknown <amincxo@gmail.com>
2024-09-29 00:04:16 +0330

    ramshtine en

---
+++ enramstin.js
@@ -0,0 +1,17 @@
+const enramstin = 
+    {
+        name: 'Rammstein',
+        genre: 'Rock',
+        image: 'artists/Rammstein.jpg',
+        songs: [
+            {  id:1 , name: 'Engel', artist: 'Rammstein', src: "rammsteinengel.mp3" , format: 'mp3' , cover: 'covers/EngelRammstein.jpg' },
+            {  id:2 , name: 'feuer frei', artist: 'Rammstein', src: "rammsteinfeuer_frei.mp3" , format: 'mp3' , cover: 'covers/Feuerfrei.jpg' },
+            {  id:3 , name: 'ich will', artist: 'Rammstein', src: "rammstineich_will.mp3" , format: 'mp3' , cover: 'covers/ichwillramshtine.jpg' },
+            
+        ]
+    }
+   
+;
+
+module.exports = enramstin;
+
