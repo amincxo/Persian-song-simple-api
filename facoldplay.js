b04960dccead7a9af7355c6ad1bf43b06f2dba03
unknown <amincxo@gmail.com>
2024-09-29 00:03:07 +0330

    fa albomcloud song

---
+++ facoldplay.js
@@ -0,0 +1,18 @@
+
+const facoldplay = 
+    {
+        name: 'بازی سرد',
+        genre: 'Pop',
+        image: 'artists/coldplay.jpg',
+        songs: [
+            {  id:1 , name: 'درستش کن', artist: 'بازی سرد', src: "ColdplayFixYou.mp3" , format: 'mp3' , cover: 'covers/ColdplayFixYou.jpg' },
+            {  id:2 , name: 'زرد', artist: 'بازی سرد', src: "ColdplayYellow.mp3" , format: 'mp3' , cover: 'covers/ColdplayYellow.jpg' },
+            {  id:3 , name: 'دانشمند', artist: 'بازی سرد', src: "coldplaythescientist.mp3" , format: 'mp3' , cover: 'covers/thescientist.jpg' },
+        ]
+    }
+   
+;
+
+module.exports = facoldplay;
+
+
