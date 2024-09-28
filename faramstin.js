ae80d1627fd73cf7324ecff4161a54df536226eb
unknown <amincxo@gmail.com>
2024-09-29 00:04:33 +0330

    ramshtine fa

---
+++ faramstin.js
@@ -0,0 +1,17 @@
+const faramstin = 
+    {
+        name: 'رامشتاین',
+        genre: 'Rock',
+        image: 'artists/Rammstein.jpg',
+        songs: [
+            {  id:1 , name: 'فرشته', artist: 'رامشتاین', src: "rammsteinengel.mp3" , format: 'mp3' , cover: 'covers/EngelRammstein.jpg' },
+            {  id:2 , name: 'بدون آتش', artist: 'رامشتاین', src: "rammsteinfeuer_frei.mp3" , format: 'mp3' , cover: 'covers/Feuerfrei.jpg' },
+            {  id:3 , name: 'من می خواهم', artist: 'رامشتاین', src: "rammstineich_will.mp3" , format: 'mp3' , cover: 'covers/ichwillramshtine.jpg' },
+            
+        ]
+    }
+   
+;
+
+module.exports = faramstin;
+
