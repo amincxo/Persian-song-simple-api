c50f2361bce9934fb3ce471b4149db4506ebc79e
unknown <amincxo@gmail.com>
2024-09-29 00:02:46 +0330

    albomcloud song

---
+++ encoldplay.js
@@ -0,0 +1,18 @@
+
+const encoldplay = 
+    {
+        name: 'Coldplay',
+        genre: 'Pop',
+        image: 'artists/coldplay.jpg',
+        songs: [
+            {  id:1 , name: 'Fix You', artist: 'Coldplay', src: "ColdplayFixYou.mp3" , format: 'mp3' , cover: 'covers/ColdplayFixYou.jpg' },
+            {  id:2 , name: 'Yellow', artist: 'Coldplay', src: "ColdplayYellow.mp3" , format: 'mp3' , cover: 'covers/ColdplayYellow.jpg' },
+            {  id:3 , name: 'TheScientist', artist: 'Coldplay', src: "coldplaythescientist.mp3" , format: 'mp3' , cover: 'covers/thescientist.jpg' },
+        ]
+    }
+   
+;
+
+module.exports = encoldplay;
+
+
