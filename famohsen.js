1e0aca6aab883b839a93a66e4168d8fd2e7973e5
unknown <amincxo@gmail.com>
2024-09-29 00:03:54 +0330

    mohsen lorestani fa

---
+++ famohsen.js
@@ -0,0 +1,18 @@
+const famohsenlorestani = 
+    {
+        name: 'محسن لرستانی',
+        genre: 'پاپ',
+        image: 'artists/mohsenlorestani.jpg',
+        songs: [
+            {  id:1 , name: 'بچه سوسول', artist: 'محسن لرستانی', src: "mohsenBachesosol.mp3" , format: 'mp3' , cover: 'covers/mohsenBachesosol.jpg' },
+            {  id:2 , name: 'بچه قرتی', artist: 'محسن لرستانی', src: "MohsenLorestaniBacheGherti.mp3" , format: 'mp3' , cover: 'covers/mohsenBachegherti.jpg' },
+            {  id:3 , name: 'بچه ننه', artist: 'محسن لرستانی', src: "MohsenLorestaniBacheNane.mp3" , format: 'mp3' , cover: 'covers/MohsenLorestaniBacheNane.jpg' },
+            {  id:4 , name: 'بی کس', artist: 'محسن لرستانی', src: "MohsenLorestaniBiKas.mp3" , format: 'mp3' , cover: 'covers/bikasmohsen.jpg' },
+            {  id:5 , name: 'نگین', artist: 'محسن لرستانی', src: "MohsenLorestaniNegin.mp3" , format: 'mp3' , cover: 'covers/MohsenLorestaniNegin.jpg' },
+        ]
+    }
+   
+;
+
+module.exports = famohsenlorestani;
+
