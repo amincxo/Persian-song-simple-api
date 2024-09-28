5aeb47641ae5fdbf7445610601f0e93f52e477bf
unknown <amincxo@gmail.com>
2024-09-29 00:03:37 +0330

    mohsen lorestani en

---
+++ enmohsen.js
@@ -0,0 +1,21 @@
+
+const enmohsenlorestani = 
+    {
+        name: 'Mohsen Lorestani',
+        genre: 'Pop',
+        image: 'artists/mohsenlorestani.jpg',
+        songs: [
+            {  id:1 , name: 'Bache Sosol', artist: 'Mohsen Lorestani', src: "mohsenBachesosol.mp3" , format: 'mp3' , cover: 'covers/mohsenBachesosol.jpg' },
+            {  id:2 , name: 'Bache Gherti', artist: 'Mohsen Lorestani', src: "MohsenLorestaniBacheGherti.mp3" , format: 'mp3' , cover: 'covers/mohsenBachegherti.jpg' },
+            {  id:3 , name: 'Bache Naneh', artist: 'Mohsen Lorestani', src: "MohsenLorestaniBacheNane.mp3" , format: 'mp3' , cover: 'covers/MohsenLorestaniBacheNane.jpg' },
+            {  id:4 , name: 'Bi Kas', artist: 'Mohsen Lorestani', src: "MohsenLorestaniBiKas.mp3" , format: 'mp3' , cover: 'covers/bikasmohsen.jpg' },
+            {  id:5 , name: 'Negin', artist: 'Mohsen Lorestani', src: "MohsenLorestaniNegin.mp3" , format: 'mp3' , cover: 'covers/MohsenLorestaniNegin.jpg' },
+        ]
+    }
+   
+;
+
+module.exports = enmohsenlorestani;
+
+
+
